3e7126c5ee35229f9f36b95059b0adfe61a45550
NarendraReddy <nkadiri09@gmail.com>
2017-07-29 23:07:37 -0400

    MessageSource with MessageSource.getMessages()

--- src/org/naren/kadiri/Circle.java
+++ src/org/naren/kadiri/Circle.java
@@ -7,25 +7,37 @@ import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.support.ManagedMap;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-
 /*@Component
 @Service
 @Controller*/
 @Repository
-public class Circle implements Shape{
+public class Circle implements Shape {
 
 	private Point center;
 
 	public Point getCenter() {
 		return center;
 	}
+	
+	@Autowired
+	private MessageSource messageSource;
 
-	@Resource(name="point2")
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
+	@Resource(name = "point2")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
@@ -33,14 +45,17 @@ public class Circle implements Shape{
 	@Override
 	public void draw() {
 		System.out.println("Inside the triangle");
-		System.out.println("Center point is (" + center.getX() + "," + center.getY() + ")");
+		//System.out.println("Center point is (" + center.getX() + "," + center.getY() + ")");
+		System.out.println(messageSource .getMessage("greeting", null, "Default Greeting", null));
+		System.out.println(messageSource .getMessage("Drawing.cricle", null, "Default Greeting", null));
+		System.out.println(messageSource .getMessage("Drawing.points", new Object [] {center.getX(), center.getY()}, "Default Greeting", null));
 	}
-	
+
 	@PostConstruct
 	public void initilize() {
 		System.out.println("It's a initialize method");
 	}
-	
+
 	@PreDestroy
 	public void destroy() {
 		System.out.println("It's a initialize method");
--- src/org/naren/kadiri/DrawingApp.java
+++ src/org/naren/kadiri/DrawingApp.java
@@ -12,7 +12,9 @@ public class DrawingApp {
 		/*BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));*/
 		ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml");
 		/*Triangle triangle = (Triangle) factory.getBean("triangle");*/
-		Shape shape = (Shape) context.getBean("triangle");
+		Shape shape = (Shape) context.getBean("circle");
 		shape.draw();
+		//System.out.println(context.getMessage("greeting", null, "Default Greeting", null));
+		
 	}
 }
